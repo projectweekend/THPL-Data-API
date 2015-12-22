4e019b3285a26c87e1622d36b1626670afc9ab3a
Brian Hines <brian@projectweekend.net>
2015-12-21 20:43:14 -0600

    Fix header

--- app/middleware/auth.py
+++ app/middleware/auth.py
@@ -5,7 +5,7 @@ from app.config import API_KEY
 class APIKey(object):
 
     def process_request(self, req, res):
-        api_key = req.get_header('X-API-KEY')
+        api_key = req.get_header('X-API-Key')
         if api_key != API_KEY:
             raise falcon.HTTPUnauthorized(
                 title='Not Authorized',
