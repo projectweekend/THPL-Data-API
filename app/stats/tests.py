507e1f8621cb3c567a5873ff549c0aa2b77d1036
Brian Hines <brian@projectweekend.net>
2015-12-26 20:14:11 -0600

    All the tests

--- app/stats/tests.py
+++ app/stats/tests.py
@@ -3,6 +3,7 @@ from app.utils.testing import APITestCase
 
 
 HOURLY_STATS_ROUTE = '/v1/stats/hourly'
+DAILY_STATS_ROUTE = '/v1/stats/daily'
 VALID_START_PARAM = '2015-12-19'
 VALID_END_PARAM = '2015-12-20'
 INVALID_START_PARAM = 'not a date'
@@ -19,7 +20,8 @@ READING_TYPES = {
     'avg_temp': float,
     'max_temp': float,
     'min_temp': float,
-    'hour': str
+    'hour': str,
+    'day': str
 }
 
 
@@ -43,6 +45,31 @@ class HourlyResourceTestCase(APITestCase):
 
     def test_hourly_stats_not_found(self):
         route = '{0}/not-a-sensor-name'.format(HOURLY_STATS_ROUTE)
+        query_string = 'start={0}&end={1}'.format(VALID_START_PARAM, VALID_END_PARAM)
+        self.simulate_get(path=route, query_string=query_string)
+        self.assertEqual(self.srmock.status, falcon.HTTP_NOT_FOUND)
+
+
+class DailyResourceTestCase(APITestCase):
+
+    def test_daily_stats(self):
+        route = '{0}/home'.format(DAILY_STATS_ROUTE)
+        query_string = 'start={0}&end={1}'.format(VALID_START_PARAM, VALID_END_PARAM)
+        body = self.simulate_get(path=route, query_string=query_string)
+        self.assertEqual(self.srmock.status, falcon.HTTP_OK)
+        self.assertEqual(len(body), 1)
+        for reading in body:
+            for k, v in reading.items():
+                self.assertIsInstance(v, READING_TYPES[k])
+
+    def test_daily_stats_with_invalid_date_params(self):
+        route = '{0}/home'.format(DAILY_STATS_ROUTE)
+        query_string = 'start={0}&end={1}'.format(INVALID_START_PARAM, VALID_END_PARAM)
+        self.simulate_get(path=route, query_string=query_string)
+        self.assertEqual(self.srmock.status, falcon.HTTP_BAD_REQUEST)
+
+    def test_daily_stats_not_found(self):
+        route = '{0}/not-a-sensor-name'.format(DAILY_STATS_ROUTE)
         query_string = 'start={0}&end={1}'.format(VALID_START_PARAM, VALID_END_PARAM)
         self.simulate_get(path=route, query_string=query_string)
         self.assertEqual(self.srmock.status, falcon.HTTP_NOT_FOUND)
