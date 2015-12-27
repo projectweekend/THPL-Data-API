74bda10a3a544cb7f971f1a2b3c439eaabe72b60
Brian Hines <brian@projectweekend.net>
2015-12-26 20:10:02 -0600

    Test bad request

--- app/stats/tests.py
+++ app/stats/tests.py
@@ -35,6 +35,12 @@ class HourlyResourceTestCase(APITestCase):
             for k, v in reading.items():
                 self.assertIsInstance(v, READING_TYPES[k])
 
+    def test_hourly_stats_with_invalid_date_params(self):
+        route = '{0}/home'.format(HOURLY_STATS_ROUTE)
+        query_string = 'start={0}&end={1}'.format(INVALID_START_PARAM, VALID_END_PARAM)
+        self.simulate_get(path=route, query_string=query_string)
+        self.assertEqual(self.srmock.status, falcon.HTTP_BAD_REQUEST)
+
     def test_hourly_stats_not_found(self):
         route = '{0}/not-a-sensor-name'.format(HOURLY_STATS_ROUTE)
         query_string = 'start={0}&end={1}'.format(VALID_START_PARAM, VALID_END_PARAM)
