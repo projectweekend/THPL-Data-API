28995ad93d5d16cb9da10f52f30ae16d34c4c5c3
Brian Hines <brian@projectweekend.net>
2015-12-27 21:43:38 -0600

    Tests

--- app/stats/tests.py
+++ app/stats/tests.py
@@ -4,6 +4,7 @@ from app.utils.testing import APITestCase
 
 HOURLY_STATS_ROUTE = '/v1/stats/hourly'
 DAILY_STATS_ROUTE = '/v1/stats/daily'
+LATEST_READING_ROUTE = '/v1/sensor'
 VALID_START_PARAM = '2015-12-19'
 VALID_END_PARAM = '2015-12-20'
 INVALID_START_PARAM = 'not a date'
@@ -21,7 +22,13 @@ READING_TYPES = {
     'max_temp': float,
     'min_temp': float,
     'hour': str,
-    'day': str
+    'day': str,
+    'temp_f': float,
+    'temp_c': float,
+    'humidity': float,
+    'pressure': float,
+    'luminosity': float,
+    'logged_at': str
 }
 
 
@@ -72,4 +79,19 @@ class DailyResourceTestCase(APITestCase):
         route = '{0}/not-a-sensor-name'.format(DAILY_STATS_ROUTE)
         query_string = 'start={0}&end={1}'.format(VALID_START_PARAM, VALID_END_PARAM)
         self.simulate_get(path=route, query_string=query_string)
+        self.assertEqual(self.srmock.status, falcon.HTTP_NOT_FOUND)
+
+
+class LatestReadingTestCase(APITestCase):
+
+    def test_latest_reading(self):
+        route = '{0}/home'.format(LATEST_READING_ROUTE)
+        body = self.simulate_get(path=route)
+        self.assertEqual(self.srmock.status, falcon.HTTP_OK)
+        for k, v in body.items():
+            self.assertIsInstance(v, READING_TYPES[k])
+
+    def test_latest_reading_not_found(self):
+        route = '{0}/not-a-sensor-name'.format(LATEST_READING_ROUTE)
+        self.simulate_get(path=route)
         self.assertEqual(self.srmock.status, falcon.HTTP_NOT_FOUND)
